e2b198939ee8d261227093a818cb145bee9d6179
John Birto <projectiss@outlook.com>
2022-08-25 22:49:00 +0530

    page design done in scheme

---
+++ NewProjectWeb/Controllers/SchemeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewProjectWeb.Controllers
+{
+    public class SchemeController : Controller
+    {
+        // GET: Scheme
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
